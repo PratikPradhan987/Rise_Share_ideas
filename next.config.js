f79108fe559366edf61035881fa93e639fbdbb90
Pratik <Pratik_201800305@smit.smu.edu.in>
2022-07-11 20:48:50 +0530

    second change config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   images:{
     domains:['cdn.sanity.io',"lh3.googleusercontent.com","static.clideo.com"],
